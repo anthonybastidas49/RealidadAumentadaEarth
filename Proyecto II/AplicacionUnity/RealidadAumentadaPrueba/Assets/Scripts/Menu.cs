499247986d99194f0bf73d1f74f9a4a17ef45238
anthonybastidas49 <anthonybastidas48@gmail.com>
2018-07-30 13:25:06 -0500

    Cambios
    :D

---
+++ Proyecto II/AplicacionUnity/RealidadAumentadaPrueba/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    public void tierra()
+    {
+        SceneManager.LoadScene("Tierra");
+    }
+    public void capasTierra()
+    {
+        SceneManager.LoadScene("TierraCapas");
+    }
+    public void TeoriaTierra()
+    {
+        SceneManager.LoadScene("TeoriaTierra");
+    }
+}
